7cad1b164cc1f3266d445d401a1b6cc324a9692e
Rakib-cloud <ri1641715@gmail.com>
2022-03-11 01:26:14 +0600

    bonus work demo

--- js/app.js
+++ js/app.js
@@ -35,11 +35,14 @@ const displayContent = (text) => {
 };
 
 const switchTab = (id) => {
+  
     if (id === "posts") {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        //document.getElementById( "liked" ).innerHTML="";
     } else if (id === "liked") {
+      //document.getElementById( "liked" ).innerHTML="";
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
